7e2fb8f012efa1fabe388e1dfedd5a966c5d6cf7
nick.xue15@gmail.com <nick.xue15@gmail.com>
2020-06-18 01:11:11 +0100

    tweak huge enemy

--- BeamDefence/Models/Enemies/HugeEnemy.cs
+++ BeamDefence/Models/Enemies/HugeEnemy.cs
@@ -5,8 +5,8 @@
         const int OwnSpeed = 1;
         const int OwnHealth = 300;
         const int OwnRadius = 60;
-        const int OwnDamage = 100;
-        const int OwnScore = 150;
+        const int OwnDamage = 30;
+        const int OwnScore = 50;
 
         public HugeEnemy(int numPlayers) : base(numPlayers, OwnSpeed, OwnHealth, OwnRadius, OwnDamage, OwnScore, EnemyType.HugeEnemy)
         {
